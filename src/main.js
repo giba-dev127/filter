506709f76c56f1af644b75d3d78478bcee64deb5
Андрей Андронов <andrew270504cska@gmail.com>
2024-10-28 16:26:18 +0300

    nigger

--- src/main.js
+++ src/main.js
@@ -2,8 +2,5 @@ import { createApp } from 'vue'
 import './style.css'
 import router from './router'
 import App from './App.vue'
-import { register } from 'swiper/element'
-
-register()
 
 createApp(App).use(router).mount('#app')
